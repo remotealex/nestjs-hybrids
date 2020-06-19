123f9a2009d2c7638b661f7ee738292f14149f4f
Alex Price <hi@alexpriceonline.com>
2020-06-19 18:30:55 +0100

    Added 'proper' user checking during auth and adding user object to req

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,29 @@
 import { Controller, Get, Render, UseGuards } from '@nestjs/common';
 import { AuthGuard } from './auth/auth.guard';
 import { User } from './auth/user.decorator';
+import { Public } from './auth/public.decorator';
 
 @Controller()
+@UseGuards(AuthGuard)
 export class AppController {
   @Get()
   @Render('index')
-  homeView() {
-    return { title: 'Home page' };
+  @Public()
+  homeView(@User() user) {
+    return { title: 'Home page', user };
   }
 
   @Get('about')
   @Render('about')
-  aboutView() {
-    return { title: 'About page' };
+  @Public()
+  aboutView(@User() user) {
+    return { title: 'About page', user };
   }
 
-  @UseGuards(AuthGuard)
   @Get('profile')
   @Render('profile')
   profileView(@User() user) {
-    console.log('user', user); // TODO get the user from the "db"
-    return { title: 'Your profile', user: { name: 'Alex Price' } };
+    console.log(user);
+    return { title: 'Your profile', user };
   }
 }
